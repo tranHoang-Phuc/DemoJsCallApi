719dcfb32b33a9c9b76cbf1a4502a3c006669d3a
Tran Hoang Phuc <phucth115.dev@gmail.com>
2025-02-10 21:10:16 +0700

    Add project files.

---
+++ DemoJsCallApi/Controllers/DemoController.cs
@@ -0,0 +1,56 @@
+﻿using DemoJsCallApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoJsCallApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly MySaleDBContext _context;
+
+        public DemoController(MySaleDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET -  JSON - AJAX
+
+        // POST - JSON -AJAX
+
+        // PUT - JSON - AJAX
+
+        // DELETE - JSON - AJAX
+
+
+        // GET -  XML - AJAX
+
+        // POST - XML -AJAX
+
+        // PUT - XML - AJAX
+
+        // DELETE - XML - AJAX
+
+
+        // File Upload - AJAX - PhucTH
+        // File Upload - FETCH - PhucTH
+
+        // GET -  JSON - FETCH
+
+        // POST - JSON - FETCH
+
+        // PUT - JSON - FETCH
+
+        // DELETE - JSON - FETCH
+
+
+        // GET -  XML - FETCH
+
+        // POST - XML - FETCH
+
+        // PUT - XML - FETCH
+
+        // DELETE - XML - FETCH
+
+    }
+}
---
+++ DemoJsCallApi/Program.cs
@@ -0,0 +1,33 @@
+using DemoJsCallApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<MySaleDBContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add CORS policy
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// use CORS policy
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
