496d5fb2364e7499f1d576f8601d762be90e8ed8
Le Hong Phuc <phuclhhe176850@fpt.edu.vn>
2025-03-01 21:55:45 +0700

    feat: api json ajax

---
+++ DemoJsCallApi/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoJsCallApi.Dtos
+{
+    public class ProductDto
+    {
+        public string? ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public int? UnitsInStock { get; set; }
+        public string? Image { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
