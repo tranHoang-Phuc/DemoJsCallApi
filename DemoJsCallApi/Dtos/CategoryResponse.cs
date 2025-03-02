ecaefab41054243a0ab52b1941439926f269aa27
Tran Hoang Phuc <phucth115.dev@gmail.com>
2025-03-02 21:00:07 +0700

    feature: xml endpoints

---
+++ DemoJsCallApi/Dtos/CategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoJsCallApi.Dtos
+{
+    public class CategoryResponse
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ DemoJsCallApi/Dtos/ProductResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoJsCallApi.Dtos
+{
+    public class ProductResponse
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Image { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public CategoryResponse Category { get; set; }
+    }
+}
